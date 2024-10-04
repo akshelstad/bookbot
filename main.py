3b4681c6af9da237d0572f8dfc73835ab8ddd968
akshelstad <akshelstad@gmail.com>
2024-10-03 20:24:11 -0600

    1. update count words function in main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    # print(text)
+    word_count = count_words(text)
+    print(f"{word_count} words found in this document")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+main()
