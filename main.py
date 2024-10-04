97d90fe8fc34eadc80ca6831fc3aeb0db4122d94
akshelstad <akshelstad@gmail.com>
2024-10-04 10:55:03 -0600

    Update main.py - add char count func

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
+    # print(chars_dict)
+    chars_dict_list = convert_dict_to_list(chars_dict)
+    # print(chars_dict_list)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document", "\n")
 
+    for dict in chars_dict_list:
+        print(f"The '{dict["name"]}' character was found {dict["num"]} times") if dict["name"].isalpha() else next
 
 def get_num_words(text):
     words = text.split()
@@ -25,5 +32,18 @@ def get_chars_dict(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def sort_on(dict):
+    return dict["num"]
+
+def convert_dict_to_list(dict):
+    chars_dict_list = []
+    
+    for char, val in dict.items():
+        chars_dict_list.append({"name" : char, "num" : val})
+
+    chars_dict_list.sort(reverse=True, key=sort_on)
+
+    return chars_dict_list
 
 main()
